eba59cddbed0749664ea26aae840877a337466dd
yokikathir <47917616+yokikathir@users.noreply.github.com>
2020-04-07 08:29:30 +0530

    first commit

--- app/src/main/java/com/example/blurdetect/java/MainActivity_java.java
+++ app/src/main/java/com/example/blurdetect/java/MainActivity_java.java
@@ -17,7 +17,6 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
